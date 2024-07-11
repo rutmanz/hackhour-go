e72330b9e3723329b518d2a8dee034c43dfd984f
Zach Rutman <zarutman+git@gmail.com>
2024-07-11 16:36:58 -0700

    fix: remove button to reduce wrapping

--- pkg/slack/client.go
+++ pkg/slack/client.go
@@ -23,7 +23,7 @@ func (c *HackHourSlackClient) CheckAuth() (*slack.AuthTestResponse, error) {
 	return c.slack.AuthTest()
 }
 
-func (c *HackHourSlackClient) GetHackHourClient() (*api.HackHourClient) {
+func (c *HackHourSlackClient) GetHackHourClient() *api.HackHourClient {
 	return c.hhClient
 }
 func (c *HackHourSlackClient) SendToSessionThread(msg string, url string) (channel string, ts string, err error) {
@@ -33,17 +33,17 @@ func (c *HackHourSlackClient) SendToSessionThread(msg string, url string) (chann
 		return
 	}
 	blocks := []slack.Block{
-		&slack.SectionBlock{
-			Type: slack.MBTSection,
-			Text: slack.NewTextBlockObject(slack.MarkdownType, msg, false, false),
-			Accessory: &slack.Accessory{
-				ButtonElement: &slack.ButtonBlockElement{
-					Type: slack.METButton,
-					Text: slack.NewTextBlockObject(slack.PlainTextType, "Open in Github", false, false),
-					URL:  url,
-				},
-			},
-		},
+		// &slack.SectionBlock{
+		// 	Type: slack.MBTSection,
+		// 	Text: slack.NewTextBlockObject(slack.MarkdownType, msg, false, false),
+		// 	Accessory: &slack.Accessory{
+		// 		ButtonElement: &slack.ButtonBlockElement{
+		// 			Type: slack.METButton,
+		// 			Text: slack.NewTextBlockObject(slack.PlainTextType, "Open in Github", false, false),
+		// 			URL:  url,
+		// 		},
+		// 	},
+		// },
 	}
 	if url != "" {
 		_, _, err = c.slack.PostMessage("C06SBHMQU8G", slack.MsgOptionText(msg, false), slack.MsgOptionBlocks(blocks...), slack.MsgOptionTS(session.MessageTs))
